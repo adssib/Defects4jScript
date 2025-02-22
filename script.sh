edaff68d3ea0370494e755ecd456b3a8ecbf22ce
adib <adibakkari@gmail.com>
2025-02-21 22:22:56 -0500

    adding color codding and stuff

--- script.sh
+++ script.sh
@@ -3,10 +3,23 @@
 # Exit script on error
 set -e
 
+# Colors
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+BLUE='\033[1;34m'
+CYAN='\033[1;36m'
+WHITE='\033[1;37m'
+NC='\033[0m' # No color
+
 # Ensure bzip2 is installed
+echo -e "${YELLOW}INFO:${NC} checking is wanted packages are installed"
+
 if ! command -v bzip2 &> /dev/null; then
-    echo "Installing bzip2..."
+    echo ""
+    echo -e "${RED}Warning:${NC} 'bzip2' is not installed!"
     sudo apt-get install -y bzip2
 else 
-    echo "bzip2 is already nstalled"
+    echo ""
+    echo -e "${GREEN}INFO:${NC} 'bzip2' is intalled"
 fi
