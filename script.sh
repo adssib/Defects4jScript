e0e72f6a26b5687bb71e15d4f5ab309ebc76f260
adib <adibakkari@gmail.com>
2025-02-22 20:09:25 -0500

    adding a way to check for unavailble packages and install then + checking is user is sudo or not

--- script.sh
+++ script.sh
@@ -1,23 +1,8 @@
 #!/bin/bash
-
 # if we are adding a '-x' if will print some intresting results 
-
 # Exit script on error
 set -e
 
-if [ "@?" -ne 0] ; then 
-    log ERROR "no parameters were supplied"
-    help()
-fi 
-
-for arg in "$@";
-do
-    case "$arg" in
-        -h) help()
-
-    esac
-done 
-# Colors
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
@@ -26,10 +11,6 @@ CYAN='\033[1;36m'
 WHITE='\033[1;37m'
 NC='\033[0m'
 
-help() {
-    log INFO "idk how to use my script either" 
-    exit 1
-}
 log() {
     local level=$1
     local message=$2
@@ -44,17 +25,62 @@ log() {
     esac
 }
 
-check_package() {
+if [ "$EUID" -ne 0 ]
+  then log ERROR "Please run as root"
+  exit
+fi
+
+targetDefects4j="/home/adssib/345/defects4j"  # Fixed path
+checkPackage() {
     local package=$1
     if ! command -v "$package" &> /dev/null; then
         log WARNING "'$package' is not installed!"
-        log LOADING "Installing '$package'..."
-        sudo apt-get install -y "$package"
-        log SUCCESS "'$package' installed successfully!"
     else
         log SUCCESS "'$package' is already installed"
     fi
 }
 
-log INFO "Checking required packages..."
-check_package bzip2
+
+doctor() {
+    log INFO "Checking required packages..."
+    checkPackage bzip2
+    checkPackage defects4j
+}
+
+fixPackages() {
+    local package=$1
+    if [[ "$package" == "defects4j" ]]; then
+        log WARNING "defects4j is not installed" 
+        cd "$targetDefects4j" || { log ERROR "Failed to change directory to $targetDefects4j"; exit 1; }
+        log INFO "going inside of '$PWD'"
+        log LOADING "initialting the script" 
+        ./init.sh
+    else
+        log LOADING "Installing '$package'..."
+        sudo apt-get install -y "$package"
+        log SUCCESS "'$package' installed successfully!"
+        log SUCCESS "'$package' is already installed"
+    fi
+}
+
+comandHelp() {
+    log INFO "idk how to use my script either" 
+    exit 1
+}
+
+if [ "$#" -eq 0 ]; then 
+    echo "Arguments received: $# -> $@"
+    log ERROR "no parameters were supplied"
+    comandHelp
+fi
+
+for arg in "$@";
+do
+    case "$arg" in
+        -h) help ;;
+        --doctor) doctor ;;
+        -d) doctor ;;
+        # -d --fix) 
+        *) help ;;
+    esac
+done 
